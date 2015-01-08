7d365d632afa760fab383fdce8efb0b32f3988e6
David Négrier <d.negrier@thecodingmachine.com>
2015-01-08 12:23:29 +0100

    Finishing migration to Splash 5

--- src/Mouf/Integration/Drupal/Druplash/DrupalTemplate.php
+++ src/Mouf/Integration/Drupal/Druplash/DrupalTemplate.php
@@ -5,6 +5,7 @@ use Mouf\Html\Utils\WebLibraryManager\WebLibraryManager;
 
 use Mouf\Html\HtmlElement\HtmlBlock;
 use Mouf\Html\Template\TemplateInterface;
+use Mouf\Html\Template\BaseTemplate\BaseTemplate;
 
 
 /**
@@ -14,78 +15,29 @@ use Mouf\Html\Template\TemplateInterface;
  * 
  * @author David Négrier
  */
-class DrupalTemplate implements TemplateInterface {
+class DrupalTemplate extends BaseTemplate {
 	
-	protected $title;
 	/**
 	 * True if the toHtml method has been called, false otherwise.
 	 * @var boolean
 	 */
 	protected $displayTriggered = false;
-	
-	/**
-	 * The weblibrarymanager is in charge of handing JS files.
-	 *
-	 * @var WebLibraryManagerInterface
-	 */
-	private $webLibraryManager;
-	
-	/**
-	 * 
-	 * @var HtmlBlock
-	 */
-	private $contentBlock;
 	    
-	public function getContentBlock() 
-	{
-	  return $this->contentBlock;
-	}
+ 	public function getContentBlock() 
+ 	{
+ 	  return $this->content;
+ 	}
 	
-	/**
-	 * The content of the page is represented by this object.
-	 * Using this object, you can add content to your page.
-	 * 
-	 * @param HtmlBlock $value
-	 */
-	public function setContentBlock($value) 
-	{
-	  $this->contentBlock = $value;
-	}
-	
-	/**
-	 * Sets the title for the page.
-	 * 
-	 * @return TemplateInterface
-	 */
-	public function setTitle($title) {
-		$this->title = $title;
-		return $this;
-	}
-	
-	public function getTitle() {
-		return $this->title;
-	}
-	
-	/**
-	 * Returns the WebLibraryManager object that can be used to add JS/CSS files to this template.
-	 *
-	 * @return WebLibraryManager
-	*/
-	public function getWebLibraryManager() {
-		return $this->webLibraryManager;
-	}
-	
-	/**
-	 * Sets the web library manager for this template.
-	 *
-	 * @Property
-	 * @param WebLibraryManager $webLibraryManager
-	 * @return BaseTemplate
-	 */
-	public function setWebLibraryManager(WebLibraryManager $webLibraryManager) {
-		$this->webLibraryManager = $webLibraryManager;
-		return $this;
-	}
+ 	/**
+ 	 * The content of the page is represented by this object.
+ 	 * Using this object, you can add content to your page.
+ 	 * 
+ 	 * @param HtmlBlock $value
+ 	 */
+ 	public function setContentBlock($value) 
+ 	{
+ 	  $this->content = $value;
+ 	}
 	
 	/**
 	 * Tells Drupal that the content should be rendered into the theme.
@@ -99,7 +51,7 @@ class DrupalTemplate implements TemplateInterface {
 		// Let's register the template renderer in the default renderer.
 		$this->getDefaultRenderer()->setTemplateRenderer($this->getTemplateRenderer());
 		
-		drupal_set_title($this->title);
+		drupal_set_title($this->getTitle());
 		$this->displayTriggered = true;
 	}
 	
